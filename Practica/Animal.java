058c6f3749bd42a984fdce11479cffd6b40af9d9
AniPoddar <98313464+AniPoddar@users.noreply.github.com>
2022-01-26 22:45:21 +0530

    Add files via upload

---
+++ Practica/Animal.java
@@ -0,0 +1,17 @@
+package aniruddha_code;
+
+ class Animal {
+	void eat(){System.out.println("eating...");}  
+	}  
+	class Dog1 extends Animal{  
+	void bark(){System.out.println("barking...");}  
+	}  
+	class TestInheritance{  
+	public static void main(String args[]){  
+	Dog1 d=new Dog1();  
+	d.bark();  
+	d.eat();  
+	}}
+ 
+
+
