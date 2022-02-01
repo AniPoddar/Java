d7a9c0c33aa853333f0f9d141e26ab51e8218d8e
AniPoddar <98313464+AniPoddar@users.noreply.github.com>
2022-02-01 14:59:09 +0530

    Update ImmutableDemo.java

--- Practica/ImmutableDemo.java
+++ Practica/ImmutableDemo.java
@@ -3,6 +3,7 @@ package aniruddha_code;
 public class ImmutableDemo {
 	public static void main(String args[]){
 		String a = "Aniruddha"; //Creating a String
+		a.concat("Poddar");
 		a=a.concat("Poddar"); //Calling a method *concat(used for appendind at the end) but still not modifiable since, the string is immutable
 		System.out.println(a);
 	}
