87ad206197917959f15c7f2a29dbb4333adb0b81
AniPoddar <98313464+AniPoddar@users.noreply.github.com>
2022-02-01 14:54:56 +0530

    Add files via upload

---
+++ Practica/ImmutableDemo.java
@@ -0,0 +1,10 @@
+package aniruddha_code;
+
+public class ImmutableDemo {
+	public static void main(String args[]){
+		String a = "Aniruddha"; //Creating a String
+		a=a.concat("Poddar"); //Calling a method *concat(used for appendind at the end) but still not modifiable since, the string is immutable
+		System.out.println(a);
+	}
+
+}
