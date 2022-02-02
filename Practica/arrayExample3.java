fb819cf702e32b06847be04e0d79c2276082fda0
AniPoddar <98313464+AniPoddar@users.noreply.github.com>
2022-02-02 16:19:11 +0530

    Add files via upload

---
+++ Practica/arrayExample3.java
@@ -0,0 +1,15 @@
+package aniruddha_code;
+
+public class arrayExample3 {
+
+	public static void main(String[] args) {
+			int a[]={1,2,3,4,5};//directly initializing and instantiating
+			for(int i:a)//for each loop prints the array 
+				System.out.println(i);
+
+		}
+		
+
+	}
+
+
