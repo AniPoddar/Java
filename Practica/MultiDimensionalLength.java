fbe418b1d517b6e05305b6f1acd6f5cb841d69f4
AniPoddar <98313464+AniPoddar@users.noreply.github.com>
2022-02-03 14:56:59 +0530

    Add files via upload

---
+++ Practica/MultiDimensionalLength.java
@@ -0,0 +1,14 @@
+package aniruddha_code;
+
+public class MultiDimensionalLength {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] a = { {1,2,3},{4,5,6}, {7,5,9} };
+		System.out.println("length of row 1 :" +a[0].length);
+		System.out.println("length of row 2 :" +a[1].length);
+		System.out.println("length of row 3 :" +a[2].length);
+
+	}
+
+}
