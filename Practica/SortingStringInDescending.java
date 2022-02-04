c9f731c5738042333d06261ec16315ad80d1126e
AniPoddar <98313464+AniPoddar@users.noreply.github.com>
2022-02-04 16:10:44 +0530

    Add files via upload

---
+++ Practica/SortingStringInDescending.java
@@ -0,0 +1,17 @@
+package aniruddha_code;
+
+import java.util.Arrays;
+
+
+public class SortingStringInDescending {
+
+	public static void main(String[] args) {
+
+		String [] array = {"Mango","Apple","Grapes","Papaya","Pineapple","Bannana","Orange"};   
+		   
+		Arrays.sort(array);  
+		System.out.println("Array elements in descending order: " +Arrays.toString(array));
+
+	}
+
+}
