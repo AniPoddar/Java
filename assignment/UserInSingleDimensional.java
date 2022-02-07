75ee8261f277dc996119518bfb8b795db0a90943
AniPoddar <98313464+AniPoddar@users.noreply.github.com>
2022-02-07 13:27:56 +0530

    Add files via upload

---
+++ assignment/UserInSingleDimensional.java
@@ -0,0 +1,24 @@
+package aniruddha_code;
+
+import java.util.Scanner;
+
+public class UserInSingleDimensional {
+
+	public static void main(String[] args) {
+		int n;  
+		Scanner s = new Scanner(System.in);  
+		System.out.print("Enter the number of elements: ");  
+		n = s.nextInt();  
+		int a[] = new int[n];  
+		System.out.println("Enter the elements of the array: "); 
+		
+		for (int i = 0; i < n; i++)   
+		{  
+		a[i] = s.nextInt();
+		}
+		for (int i = 0; i < a.length; i++)   
+		{       
+		System.out.println ( a[i]); 
+
+	}
+	}}
