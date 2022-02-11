f3c2313efc01d7bd73b227d00d04b59040b16d1f
AniPoddar <98313464+AniPoddar@users.noreply.github.com>
2022-02-11 13:59:03 +0530

    Add files via upload

---
+++ Practica/Collection/TreeSetExample1.java
@@ -0,0 +1,23 @@
+package aniruddha_code;
+import java.util.*;
+
+public class TreeSetExample1 {
+
+	public static void main(String[] args) {
+		//Creating and adding elements  
+		TreeSet<String> set=new TreeSet<String>();  
+		set.add("Ravi");  
+		set.add("Vijay");  
+		set.add("Ravi");  
+		set.add("Ajay");  
+		//traversing elements  
+		Iterator<String> itr=set.iterator();  
+		while(itr.hasNext()){  
+		System.out.println(itr.next());  
+		}  
+		}  
+
+
+	}
+
+
