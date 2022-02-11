ba4c7cb801cf9e0dffa8a09e3c221f6e22b3a2d7
AniPoddar <98313464+AniPoddar@users.noreply.github.com>
2022-02-11 10:25:03 +0530

    Add files via upload

---
+++ Practica/Collection/EqualsAndHashCodeMethod.java
@@ -0,0 +1,28 @@
+package aniruddha_code;
+
+
+class EqualsAndHashCodeMethod {
+	public String name;
+    public int id;
+	public EqualsAndHashCodeMethod(String name, int id) {
+		super();
+		this.name = name;
+		this.id = id;
+	}
+	@Override
+	public int hashCode() {
+	return this.id;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj)
+			return true;
+		if(obj == null || obj.getClass()!= this.getClass())
+            return false;
+		EqualsAndHashCodeMethod e = (EqualsAndHashCodeMethod) obj;
+		return(e.name == this.name && e.id == this.id);
+	}
+    
+
+
+}
