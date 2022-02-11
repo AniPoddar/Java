f7343fa09830c839b02cf43369d3f0d1c605d466
AniPoddar <98313464+AniPoddar@users.noreply.github.com>
2022-02-11 10:26:33 +0530

    Add files via upload

---
+++ Practica/Collection/Practical1.java
@@ -0,0 +1,23 @@
+package aniruddha_code;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class Practical1 {
+
+	public static void main(String[] args) {
+		ArrayList<String> list=new ArrayList<String>(); 
+		list.add("Aniruddha"); 
+		list.add("Anju");  
+		list.add("Koushik");
+		list.add("Utkarsha");
+		list.add("");
+		Collections.sort(list);
+		Iterator<String> itr=list.iterator();
+		while(itr.hasNext()){  
+		System.out.println(itr.next());
+
+	}
+
+}}
