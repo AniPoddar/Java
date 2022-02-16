1b007b7bcbbee81131f93418d8da28fbe4176b0d
AniPoddar <98313464+AniPoddar@users.noreply.github.com>
2022-02-16 14:49:13 +0530

    Add files via upload

---
+++ Practica/MySql/Student.sql
@@ -0,0 +1,20 @@
+Create database Aniruddha;
+
+Use Aniruddha;
+
+Create table Student(
+id int primary key not null, 
+name varchar(30) not null,
+address varchar(40) not null
+);
+
+select * from Student;
+
+insert into Student values(1, 'Aniruddha', '4/108, Nelinagar');
+
+insert into Student values(2, 'Koushik', '6/11, Kolkata');
+
+insert into Student values(3, 'Utkarsha', '3/18, Maharastra');
+
+
+
