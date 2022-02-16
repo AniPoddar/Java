85ff539c7a7357c3707c93dc9ccb075e50392696
AniPoddar <98313464+AniPoddar@users.noreply.github.com>
2022-02-16 22:48:00 +0530

    Update Student.sql

--- Practica/MySql/Student.sql
+++ Practica/MySql/Student.sql
@@ -25,6 +25,8 @@ JOINING_DATE datetime,
  DEPARTMENT varchar(20)
  );
  
+ Select * from Worker;
+ 
 insert into Worker values(001, 'Monika', 'Arora', 100000, '2014-02-20 09:00:00', 'HR');
 insert into Worker values(002, 'Niharika', 'Verma', 80000, '2014-06-11 09:00:00', 'Admin');
 insert into Worker values(003, 'Vishal', 'Singhal',	300000, '2014-02-20 09:00:00', 'HR');
@@ -34,7 +36,7 @@ insert into Worker values(006, 'Vipul', 'Diwan', 200000, '2014-06-11 09:00:00', 
 insert into Worker values(007, 'Satish', 'Kumar', 75000, '2014-01-20 09:00:00', 'Account');
 insert into Worker values(008, 'Geetika', 'Chauhan', 90000, '2014-04-11 09:00:00', 'Admin');
 
- Select * from Worker;
+ 
 
 Create table Bonus(
 WORKER_REF_ID int not null,
